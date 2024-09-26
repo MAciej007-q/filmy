d0c3797a71fd93d7c40899ad648c33ab073eeabb
DESKTOP-6ONT81G\Blar2TP <maciej13dd@gmail.com>
2024-09-26 12:33:18 +0200

    pole edycyjne i jego etykieta o treści „Tytuł filmu”

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <div class="mb-3">
+            <label for="movie" class="form-label">Tytuł Filmu</label>
+            <input type="text" class="form-control" id="movie" aria-describedby="emailHelp" />
+          </div>
+        </form>
       </header>
     </div>
   );
